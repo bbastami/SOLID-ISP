893b232a4c4c538ca6cb013531c130f576c54dd8
Behnam Bastami <behnam.bastami@activenetwork.com>
2014-06-17 20:08:39 -0700

    initial commit

---
+++ BadDesign/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadDesign
+{
+    public class ObjectA
+    {
+        int aProp;
+    }
+
+    public class ObjectB
+    {
+        string bProp;
+    }
+
+    public class bloatedDBService
+    {
+        public void addObjectA(ObjectA oa)
+        { 
+            // code to add oa to DB
+        }
+
+        public void removeObjectA(ObjectA oa)
+        {
+            // code to remove oa from DB
+        }
+
+        public void addObjectB(ObjectB ob)
+        {
+            // code to add ob to DB
+        }
+
+        public void removeObjectB(ObjectB ob)
+        {
+            // code to remove ob from DB
+        }
+
+        public List<ObjectA> findAllObjectA() { return new List<ObjectA>(); }
+        public List<ObjectB> findAllObjectB() { return new List<ObjectB>(); }
+        public List<ObjectA> findAllObjectAWithIntPropEqualZero() { return new List<ObjectA>(); }
+        public List<ObjectB> findAllObjectBWithStrPropEmpty() { return new List<ObjectB>(); }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BetterDesign/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDesign
+{
+    public class ObjectA
+    {
+        int aProp;
+    }
+
+    public class ObjectB
+    {
+        string bProp;
+    }
+
+    public class ObjectC
+    {
+        List<int> cProp;
+    }
+
+    public class baseDBService<T>
+    {
+        public void add(T obj);
+        public void remove(T obj);
+        public List<T> findAll();
+    }
+
+    public class objectADBService : baseDBService<ObjectA>
+    {
+        public List<ObjectA> findAllObjectAWithIntPropEqualZero() { return new List<ObjectA>(); }
+    }
+
+    public class objectBDBService : baseDBService<ObjectB>
+    {
+        public List<ObjectB> findAllObjectBWithStrPropEmpty() { return new List<ObjectB>(); }
+    }
+
+    public class objectCDBService : baseDBService<ObjectC>
+    {
+        public List<ObjectC> findAllObjectCWithListPropEmpty() { return new List<ObjectC>(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
